ef3658e57b7ae5a903df24e054b89a18ab9b5201
bertrandrusa <142926167+bertrandrusa@users.noreply.github.com>
2024-05-22 01:38:59 -0500

    Add files via upload

---
+++ include/csc232.h
@@ -0,0 +1,39 @@
+/**
+ * CSC232 - Data Structures
+ * Missouri State University, Fall 2020
+ *
+ * @file    csc232.h
+ * @author  Jim Daehn <jdaehn@missouristate.edu>
+ * @brief   Macros, libraries and definitions for use in csc232 assignments.
+ */
+
+#ifndef MSU_CSC232_H__
+#define MSU_CSC232_H__
+
+#define FALSE 0
+#define TRUE 1
+#define EXECUTE_BLOCK FALSE
+
+#include <cassert>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <utility>
+#include <vector>
+
+#include "expanded_templates.h"
+
+/** Common iostream objects */
+using std::cout;
+using std::cerr;
+using std::cin;
+using std::endl;
+
+/** Common iomanip objects */
+using std::setw;
+using std::right;
+using std::left;
+using std::setprecision;
+
+#endif // MSU_CSC232_H__
---
+++ include/expanded_templates.h
@@ -0,0 +1,20 @@
+/**
+ * CSC232 - Data Structures
+ * Missouri State University, Fall 2020
+ *
+ * @file    expanded_templates.h
+ * @author  Jim Daehn <jdaehn@missouristate.edu>
+ * @brief   Class template expansions.
+ */
+
+#ifndef MSU_CSC232_EXPANDED_TEMPLATES_H__
+#define MSU_CSC232_EXPANDED_TEMPLATES_H__
+
+#include <string>
+#include "STLQueueToQueueAdapter.cpp"
+
+template class STLQueueToQueueAdapter<std::string>;
+
+/* Include class template implementation files as needed */
+
+#endif // MSU_CSC232_EXPANDED_TEMPLATES_H__
